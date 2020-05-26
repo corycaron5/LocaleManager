d0ff8e624f5c4de1e2b4d6ee597008b7d5a25e05
corycaron5 <corycaron5@gmail.com>
2020-05-26 15:27:36 -0400

    Updated to use Dependency Injection

--- src/me/cory/localization/LoginEvent.java
+++ src/me/cory/localization/LoginEvent.java
@@ -7,6 +7,16 @@ import org.bukkit.event.player.PlayerLoginEvent;
 
 public class LoginEvent implements Listener{
 
+	private LocaleManager localeMan;
+	
+	/**
+	 * Constructor with LocaleManager instance
+	 * @param localeMan LocaleManager for this plugin
+	 */
+	public LoginEvent(LocaleManager localeMan){
+		this.localeMan = localeMan;
+	}
+	
 	/**
 	 * Register player's locale or default if they have never joined before
 	 * We register it in the Login event so it's already loaded if we want to send a message on the join event
@@ -15,12 +25,12 @@ public class LoginEvent implements Listener{
 	@EventHandler
 	public void onPlayerLogin(PlayerLoginEvent event){
 		Player player = event.getPlayer();
-		LocaleManager man = Main.getPlugin(Main.class).getLocaleManager();
-		if(!man.containsPlayer(player.getUniqueId())){
+		if(!localeMan.containsPlayer(player.getUniqueId())){
 			String locale = player.getLocale();
-			if(man.containsLocale(locale))man.setPlayerLocale(player.getUniqueId(), locale);
-			else man.setPlayerLocale(player.getUniqueId(), man.getDefaultLocale());
+			if(localeMan.containsLocale(locale))localeMan.setPlayerLocale(player.getUniqueId(), locale);
+			else localeMan.setPlayerLocale(player.getUniqueId(), localeMan.getDefaultLocale());
 		}
 	}
 	
 }
+
